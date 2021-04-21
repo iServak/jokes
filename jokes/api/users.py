d6ed1a4b6b67f7911e4471a3fb7a474d024e9f5d
iServak <servak.sql@gmail.com>
2021-04-21 13:42:40 +0300

    Update users.py

--- jokes/api/users.py
+++ jokes/api/users.py
@@ -8,7 +8,6 @@ def register(data):
     is_admin = data.get("is_admin", False)
 
     user = User.query.filter(User.username == username).first()
-    print(user)
     if user is None:
         user = User(username=username, token=token, is_admin=is_admin)
         db.session.add(user)
